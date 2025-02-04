e496d6a38dd4ad9400fa479c1e1a17ef0e0501d7
Jeonhj38 <lg7737@naver.com>
2025-02-04 17:52:30 +0900

    Add files via upload

---
+++ day04/event/quiz/index.js
@@ -0,0 +1,21 @@
+import { changeLight } from "./func.js";
+
+const input = document.querySelector("#input");
+const special = document.querySelector("special");
+const charLength = document.querySelector("#charLength");
+const checkButton = document.querySelector("#checkButton");
+const specialList = [..."!@#$"];
+
+const changeLight = (target, bool) => {
+  target.style.setProperty("color", bool ? "#56BA9F" : "efefef");
+};
+
+input.addEventListener("input", (e) => {
+  const { value } = e.target;
+  const { length } = value;
+  const isLengthValid = 6 <= length && length <= 20;
+  const isSpecialValid = specialList.some((v) => value.includes(v));
+  changeLight(charLength, isLengthValid);
+  changeLight(special, isSpecialValid);
+  changeLight(checkButton, isLengthValid && isSpecialValid);
+});
---
+++ day04/module/index.js
@@ -0,0 +1,8 @@
+import { makeCard } from "./componerts/card";
+const clothes = document.querySelector("#clothes");
+
+const data = [
+  {
+    img,
+  },
+];
